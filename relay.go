1ebf59ddc608a356799964707d7bdfec9cf7b00a
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2019-01-30 19:09:44 +0900

    add []nats.Option to context

--- relay.go
+++ relay.go
@@ -24,6 +24,11 @@ func NewRelayServer(ctx context.Context) *RelayServer {
     nats.NoEcho(),
     nats.Name(UA),
   }
+  opts := ctx.Value("relay.nats-options")
+  if opts != nil {
+    r.opts = append(r.opts, opts.([]nats.Option)...)
+  }
+
   r.conf = ctx.Value("relay.config").(RelayConfig)
 
   var errorHandler nats.ErrHandler
